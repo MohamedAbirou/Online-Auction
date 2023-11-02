8ea8d9ef36ae00f1975b479276bc930d7ff8d534
Mohamed Abirou <abiroumohamed58@gmail.com>
2023-11-03 00:50:04 +0100

    glhf

--- commerce/settings.py
+++ commerce/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/auctions/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'auctions/static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 LOGIN_URL = 'login'
 
 MEDIA_URL = '/media/'
